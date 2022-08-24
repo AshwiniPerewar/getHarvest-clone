9b0c531abdce9d50d5451ec286105cf3c0f31df5
Hemant Prajapati <hemantprajapati20500@gmail.com>
2022-08-24 23:28:01 +0530

    feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { AllRoutes } from './allRoutes/AllRoutes';
 import './App.css';
+import { Features } from './pages/features/Features';
 
 function App() {
   return (
     <div className="App">
       <AllRoutes/>
+     
     </div>
   );
 }
--- src/allRoutes/AllRoutes.jsx
+++ src/allRoutes/AllRoutes.jsx
@@ -8,6 +8,7 @@ import { Navbar } from '../components/navbar/Navbar';
 import { Footer } from '../components/footer/Footer';
 import { Customers } from '../pages/customers/Customers';
 import { Integrations } from '../pages/integrations/Integrations';
+import { Features } from '../pages/features/Features';
 
 export const AllRoutes = () => {
   return (
@@ -16,6 +17,7 @@ export const AllRoutes = () => {
         <Routes>
             <Route path='/customers' element={<Customers/>} />
             <Route path='/integrations' element={<Integrations/>} />
+            <Route path='/features' element={<Features/>} />
         </Routes>
         <Footer/>     
     </div>
