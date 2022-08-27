fec4625ae981c3df5daf0b113f14b8c31964cad8
Hemant Prajapati <hemantprajapati20500@gmail.com>
2022-08-27 17:32:37 +0530

    feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { AllRoutes } from './allRoutes/AllRoutes';
 import './App.css';
-import { Report } from './pages/Reports/Report';
+
 
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Report/> */}
+    
       <AllRoutes/>
     </div>
   );
