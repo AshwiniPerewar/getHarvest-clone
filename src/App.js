8e0b83c628997949b18b2a23cbaccf13caa7b808
Hemant Prajapati <hemantprajapati20500@gmail.com>
2022-08-27 15:07:18 +0530

    event and css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { AllRoutes } from './allRoutes/AllRoutes';
 import './App.css';
+import { Compo } from './pages/features/Compo';
 import { Features } from './pages/features/Features';
 // import { Whyharvest } from './pages/wharvest/Whyharvest';
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes/>
-      {/* <Features/> */}
+      {/* <AllRoutes/> */}
+      <Features/>
+      {/* <Compo/> */}
     
     </div>
   );
