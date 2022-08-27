d5b712000197bf67991d639a5cf1ca4866391721
sushantp432 <sushantpagare55@gmail.com>
2022-08-27 15:09:46 +0530

    expenses added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { AllRoutes } from './allRoutes/AllRoutes';
 import './App.css';
-import { Features } from './pages/features/Features';
+import { DisplayExpense } from './components/expenses/DisplayExpense';
+import { EditExpense } from './components/expenses/EditExpense';
+import { Expenses } from './components/expenses/Expenses';
+import { SaveExpense } from './components/expenses/SaveExpense';
+// import { DisplayExpense } from './components/expenses/DisplayExpense';
+// import { Expenses } from './components/expenses/Expenses';
+import { Expense } from './pages/expenses/Expense';
+// import { Features } from './pages/features/Features';
 // import { Whyharvest } from './pages/wharvest/Whyharvest';
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes/>
+      {/* <AllRoutes/> */}
+     
+      <Expenses />
+      {/* <DisplayExpense /> */}
+      <SaveExpense />
+      {/* <EditExpense/> */}
       {/* <Features/> */}
     
     </div>
